b1b6db9c6c840c7a8d655957a7c116c33aefe2fa
Jon Shea <jon@sheacloud.com>
2023-01-06 13:25:00 -0500

    Add basic role assignment module

---
+++ modules/role-assignment/main.tf
@@ -0,0 +1,8 @@
+data "azurerm_subscription" "primary" {}
+
+
+resource "azurerm_role_assignment" "assignment" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = var.role_definition_name
+  principal_id         = var.principal_id
+}
---
+++ modules/role-assignment/variables.tf
@@ -0,0 +1,9 @@
+variable "role_definition_name" {
+  description = "The name of the role definition to assign to the principal."
+  type        = string
+}
+
+variable "principal_id" {
+  description = "The ID of the principal to assign the role to."
+  type        = string
+}
